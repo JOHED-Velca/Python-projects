d46fd2da5752eb83e8b086b5b600eee892c47b96
Jonathan Velasquez <johedvelca@gmail.com>
2025-10-11 17:55:08 -0400

    Custom tool added, save to a text file

--- AI-agent-tutorial/tools.py
+++ AI-agent-tutorial/tools.py
@@ -3,6 +3,22 @@ from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import Tool
 from datetime import datetime
 
+def save_to_txt(data: str, filename: str = "research_output.txt"):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    formatted_text = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n"
+    
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(formatted_text)
+    
+    return f"Data successfully saved to {filename}"
+
+# Wrap the function as a tool
+save_tool = Tool(
+    name="Save_text_to_file",
+    func=save_to_txt,
+    description="Saves the provided text data to a text file. Input should be the text data to save.",
+)
+
 search = DuckDuckGoSearchRun()
 search_tool = Tool(
     name="SearchWeb",
